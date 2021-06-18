ba41fb9048096a4b7522e1e488d66c8095279e32
DillanDev <developer.dilan@gmail.com>
2021-06-18 15:58:49 -0500

    Subiendo archivos junio 18

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+//En caso tal que el sistema operativo me de un puerto este lo agarrará
+
+//routes
+app.use(require('./routes/index'));
+
+app.listen(app.get('port'), () => {
+    console.log("Server on port", app.get('port'));
+});
